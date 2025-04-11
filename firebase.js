dd11d4c0a1f7fdf9cade0ac610e115ec9fe4786a
BicudoMonsenhor <monsenhorbicudomarilia@gmail.com>
2025-04-11 19:07:53 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBAJt0ODcZjiQ8iM9fPEb5tWlNjKyZKtWw",
+    authDomain: "ocorrencia-e5e70.firebaseapp.com",
+    databaseURL: "https://ocorrencia-e5e70-default-rtdb.firebaseio.com", // ✅ necessário para salvar os dados
+    projectId: "ocorrencia-e5e70",
+    storageBucket: "ocorrencia-e5e70.appspot.com",
+    messagingSenderId: "417013595919",
+    appId: "1:417013595919:web:4fce0d291afcbe43fd887a",
+    measurementId: "G-S3PCP1HSMT"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
